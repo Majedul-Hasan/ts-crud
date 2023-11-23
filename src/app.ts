fbf5f140fc76821ae860c6c5fff1bbc63411d875
Majedul Hasan <hasanmajedul@gmail.com>
2023-11-23 23:00:42 +0600

    db connect

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction , Application} from 'express';
 import { json } from 'body-parser';
+import connect from './config/db.config';
 
-
-const app = express();
+const app: Application = express();
 
 app.use(json());
 
+const db = 'mongodb://localhost:27017/test';
+connect({db});
 
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).json({ message: err.message });
-});
+app.get('/', (req: Request, res: Response) =>
+  res.send('Welcome to the Mongoose & TypeScript example')
+);
 
 app.listen(3000, ()=>{
   console.log("server is running")
