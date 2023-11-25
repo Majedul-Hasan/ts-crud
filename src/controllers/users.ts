12797ddc843723b61deef17708f04e5476fea256
Majedul Hasan <hasanmajedul@gmail.com>
2023-11-25 22:38:55 +0600

    getUsers ctrl

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -6,14 +6,25 @@ const createUser = async (req: Request , res:Response ) => {
   // console.log(req.body)
    const result = await UserServices.createUserIntoDB(req.body);
   //  console.log(result)
+  
     res.status(200).json({
       success: true,
       message: 'User is Created successfully',
       data: result,
     });
 }
+const getUsers = async (req: Request , res:Response ) => {
+  // console.log(req.body)
+   const result = await UserServices.getallUsersFromDB()
+  //  console.log(result)
+  
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+}
 
 
 export  {
-  createUser
+  createUser, getUsers
 };
