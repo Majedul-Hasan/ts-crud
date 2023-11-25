c7bc820c5fd2325d6aa607915563a27dc90dc196
Majedul Hasan <hasanmajedul@gmail.com>
2023-11-25 22:25:24 +0600

    /api/users change

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,12 @@ app.use(json());
 const db = 'mongodb+srv://apolo2:wnRwDO7qM7KC5o92@apolo.ua3pari.mongodb.net/RAKIB?retryWrites=true&w=majority';
 // const db = 'mongodb://localhost:27017/testY';
 connect({db});
-app.use('/api/v1', userRouter)
+app.use('/api/users', userRouter)
 
 app.get('/', (req: Request, res: Response) =>
   res.send('Welcome to the Mongoose & TypeScript example')
 );
 
 app.listen(5000, ()=>{
-  console.log("server is running")
+  console.log("server is running on port " + 5000);
 } );
