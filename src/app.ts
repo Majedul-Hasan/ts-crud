1fc6cb51a8793d933e2334877da5bfe643efe12f
Majedul Hasan <hasanmajedul@gmail.com>
2023-11-25 21:27:08 +0600

    mongo cloud

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
 import express, { Request, Response, NextFunction , Application} from 'express';
 import { json } from 'body-parser';
 import connect from './config/db.config';
+import userRouter from './routes/users';
 
 const app: Application = express();
 
 app.use(json());
 
-const db = 'mongodb://localhost:27017/test';
+const db = 'mongodb+srv://apolo2:wnRwDO7qM7KC5o92@apolo.ua3pari.mongodb.net/RAKIB?retryWrites=true&w=majority';
+// const db = 'mongodb://localhost:27017/testY';
 connect({db});
+app.use('/api/v1', userRouter)
 
 app.get('/', (req: Request, res: Response) =>
   res.send('Welcome to the Mongoose & TypeScript example')
 );
 
-app.listen(3000, ()=>{
+app.listen(5000, ()=>{
   console.log("server is running")
 } );
